074fa46db8aa4aa38777ac2afa45d101dbf1ccdd
lshang <lei.shang@hotmail.com>
2016-08-13 16:02:13 +1000

    Added a case study

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+---
+output: html_document
+---
 README
 
 R, at its heart, is a functional programming (FP) language.
 
 Reference:
  Advanced R by Hadley Wichham
+ 
 	http://adv-r.had.co.nz/Functional-programming.html
--- functionalR.R
+++ functionalR.R
@@ -1,32 +1,82 @@
 # A demo of Functional Programming in R
-# Key idea:
-#   Function composition: lapply + fix_missing
+# Key ideas:
+#   1) Function composition
+#   2) Storing functions in lists
+#   3) Closures
+
+power <- function(exponent) {
+  print(environment())
+  function(x) { x ^ exponent }
+}
+
+summary <- function(x) {
+  funs <- c(mean, median, sd) # storing functions in lists
+  lapply(funs, function(f) f(x, na.rm = TRUE))
+}
 
 fix_missing <- function(x) {
   x[x == -99] <- NA
   x
 }
 
+midpoint_composite <- function(f, a, b, n = 10) {
+  points <- seq(a, b, length = n + 1)
+  h <- (b - a) / n
+  
+  area <- 0
+  for (i in seq_len(n)) {
+    area <- area + h * f((points[i] + points[i + 1]) / 2)
+  }
+  area
+}
+
+trapezoid_composite <- function(f, a, b, n = 10) {
+  points <- seq(a, b, length = n + 1)
+  h <- (b - a) / n
+  
+  area <- 0
+  for (i in seq_len(n)) {
+    area <- area + h / 2 * (f(points[i]) + f(points[i + 1]))
+  }
+  area
+}
+
+composite <- function(f, a, b, n = 10, rule) {
+  points <- seq(a, b, length = n + 1)
+  
+  area <- 0
+  for (i in seq_len(n)) {
+    area <- area + rule(f, points[i], points[i + 1])
+  }
+  
+  area
+}
+
+# --------------------------------------------------------------
 # Generate a sample datase (-99 represents missing values)
 set.seed(1014)
 df <- data.frame(replicate(6, sample(c(1:10, -99), 6, rep = TRUE)))
 names(df) <- letters[1:6]
-# df (-99 represents a missing value)
-#    a  b c   d   e f
-# 1  1  6 1   5 -99 1
-# 2 10  4 4 -99   9 3
-# 3  7  9 5   4   1 4
-# 4  2  9 3   8   6 8
-# 5  1 10 5   9   8 6
-# 6  6  2 1   3   8 5
 
-# Apply a Function over a List/Vector/Data Frame
-df[] <- lapply(df, fix_missing) # function compositon: lapply + fix_missing
-# df (-99 replaced with NA)
-#    a  b c  d  e f
-# 1  1  6 1  5 NA 1
-# 2 10  4 4 NA  9 3
-# 3  7  9 5  4  1 4
-# 4  2  9 3  8  6 8
-# 5  1 10 5  9  8 6
-# 6  6  2 1  3  8 5
+# Apply a function over the data frame
+#   - Function composition: lapply + fix_missing
+df[] <- lapply(df, fix_missing)
+
+# Apply a function over the data frame
+#   - Storing functions in lists
+lapply(df, summary)
+
+# The simplest FP tool - the anonymous function
+integrate(function(x) sin(x), lower =  0, upper = 2 * pi)
+
+# Closures 
+#   - Parent function and child functions
+square <- power(2)
+square(2)
+
+cube <- power(3)
+cube(2)
+
+# Case study: numerical integration
+composite(sin, 0, pi, n = 10, rule = trapezoid_composite)
+composite(sin, 0, pi, n = 10, rule = midpoint_composite)
